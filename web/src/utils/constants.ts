ad43e8662b84417eb80fc423c775be4fb973ebab
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-11-08 23:37:32 +0100

    Deleted vue project

--- web/src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const taskStatuses = [
-    {key: 'todo', label: 'To Do', color: '#149eff'},
-    {key: 'in-progress', label: "In Progress", color: '#ffa115'},
-    {key: 'in-review', label: "In Review", color: '#674ffb'},
-    {key: 'completed', label: "Completed", color: '#15ff8e'}
-]
--- web/src/utils/useFetch.ts
+++
@@ -1,8 +0,0 @@
-import {createFetch} from "@vueuse/core";
-
-export const useFetch = createFetch({
-    baseUrl: 'http://localhost:8080/api',
-    fetchOptions: {
-        credentials: 'include'
-    },
-})
